8f56da6611ff8f68c9b960b40205aaa7edbc5cb2
Miloš Jovanović <milosh-96@outlook.com>
2024-01-19 02:51:46 +0100

    add dynamic sports menu;

--- SportsGlobe.Web/Controllers/MapController.cs
+++ SportsGlobe.Web/Controllers/MapController.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsGlobe.Web.Data;
+using SportsGlobe.Web.ViewModels.Map;
 
 namespace SportsGlobe.Web.Controllers
 {
     public class MapController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MapController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            MainMapViewModel viewModel = new MainMapViewModel();
+            viewModel.Sports = _context.Sports.Include(x=>x.Teams).Where(x=>x.Teams.Count > 0).ToList();
+            ViewData["Sports"] = viewModel.Sports;
             ViewData["Title"] = "Map";
-            return View();
+            return View(viewModel);
         }
     }
 }
---
+++ SportsGlobe.Web/ViewModels/Map/MainMapViewModel.cs
@@ -0,0 +1,9 @@
+﻿using SportsGlobe.Web.Domain;
+
+namespace SportsGlobe.Web.ViewModels.Map
+{
+    public class MainMapViewModel
+    {
+        public List<Sport> Sports { get; set; } = new List<Sport>();
+    }
+}
